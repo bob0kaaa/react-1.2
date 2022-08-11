84f6739a3df54e8095d00da3990fdee907b319ff
Damir Alimov <alimovdf@mail.ru>
2022-08-11 21:34:52 +0500

    DZ v.2

--- src/components/ShopItemClass.js
+++ src/components/ShopItemClass.js
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function ShopItemClass(props) {
-	const { item } = props
-	return (
-		<div className='main-content'>
-			<h2>{item.brand}</h2>
-			<h1>{item.title}</h1>
-			<h3>{item.description}</h3>
-			<div className='description'>
-				{item.descriptionFull}
+export default class ShopItemClass extends Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		const { item } = this.props;
+		return (
+			<div className='main-content'>
+				<h2>{item.brand}</h2>
+				<h1>{item.title}</h1>
+				<h3>{item.description}</h3>
+				<div className='description'>
+					{item.descriptionFull}
+				</div>
+				<div className='highlight-window mobile'><div className='highlight-overlay'></div></div>
+				<div className='divider'></div>
+				<div className='purchase-info'>
+					<div className='price'>{item.currency}{item.price}</div>
+					<button>Добавить в корзину</button>
+				</div>
 			</div>
-			<div className='highlight-window mobile'><div className='highlight-overlay'></div></div>
-			<div className='divider'></div>
-			<div className='purchase-info'>
-				<div className='price'>{item.currency}{item.price}</div>
-				<button>Добавить в корзину</button>
-			</div>
-		</div>
-	)
+		)
+	}
 }
